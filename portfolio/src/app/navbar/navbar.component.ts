4ba429e48dc979c22e2b859dc83c2c26720d7235
Gurunathan <gurunathan.p@indiumsoft.com>
2025-02-13 23:36:54 +0530

    ui completed

--- portfolio/src/app/navbar/navbar.component.ts
+++ portfolio/src/app/navbar/navbar.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-navItems:any=['Home', 'About', 'Projects','Contact']
+navItems:any=['Home', 'About', 'Projects','Contact Me']
 }
