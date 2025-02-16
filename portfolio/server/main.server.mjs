2ec3ffa4558e20d4d4ef97d9e4bce54fef7a9e8d
Gurunathan <gurunathan.p@indiumsoft.com>
2025-02-16 15:46:31 +0530

    update

--- portfolio/server/main.server.mjs
+++ portfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XF7F3ASS.mjs";import"./chunk-RFVT3FBN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-7NOMRUOS.mjs";import"./chunk-RFVT3FBN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
