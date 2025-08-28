f189195b1c2fe3affee2d9f038c4fddcf74a213a
Gurunathan <gurunathan.p@indiumsoft.com>
2025-08-28 13:18:02 +0530

    updated animation

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 function toggleSkillsLevelVisibility() {
-    const skillsLevel = document.getElementById('skills-level');
-    const toggleButton = document.getElementById('skill-toggle-button');
-    if (skillsLevel) {
-        skillsLevel.classList.toggle('d-none');
+  const skillsLevel = document.getElementById('skills-level');
+  const toggleButton = document.getElementById('skill-toggle-button'); 
+  if (skillsLevel) {
+    skillsLevel.classList.toggle('d-none');
+  }
+
+  if (toggleButton) {
+    if (toggleButton.classList.contains('bi-chevron-down')) {
+      toggleButton.classList.remove('bi-chevron-down');
+      toggleButton.classList.add('bi-chevron-up');
+    } else {
+      toggleButton.classList.remove('bi-chevron-up');
+      toggleButton.classList.add('bi-chevron-down');
     }
-    if (toggleButton) {
-        let icon = toggleButton.querySelector('.show-skill-btn');
-        if (icon) {
-            console.log('Toggling icon class');
-            if (icon.classList.contains('bi-chevron-down')) {
-                icon.classList.remove('bi-chevron-down');
-                icon.classList.add('bi-chevron-up');
-            } else {
-                icon.classList.remove('bi-chevron-up');
-                icon.classList.add('bi-chevron-down');
-            }
-        }
-    }
+  }
 }
+
+
+const items = document.querySelectorAll(".exp-item");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const item = entry.target;
+    if (entry.isIntersecting) {
+      const index = Array.from(items).indexOf(item);
+      setTimeout(() => item.classList.add("show"), index * 200);
+    } 
+    // else {
+    //    setTimeout(() => item.classList.remove("show"),index * 200);
+    // }
+  });
+}, { threshold: 0.3 });
+
+items.forEach(item => observer.observe(item));
